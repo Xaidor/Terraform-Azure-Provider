02031f1924f7c77f0dc3e768ee60bf76ac9ee9a3
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2024-12-22 16:51:13 -0500

    VPN + security module added

--- provider.tf
+++ provider.tf
@@ -10,4 +10,17 @@ terraform {
 provider "azurerm" {
   resource_provider_registration = "none"
   features {}
+  subscription_id = 
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.region
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
 }
